4563da3245ff954105a9a414d664a11d9cb58ae1
tkDev <tkdev0101@gmail.com>
2025-01-21 21:31:43 +0200

    2nd commit
    Updated urls and views
    created template
    added a form
    basic submission of task works

--- todos/todoapp/models.py
+++ todos/todoapp/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+
 class Mytodo(models.Model):
     task = models.CharField(max_length=50)
 
-def __str__(self):
-    return self.task
+    def __str__(self):
+        return self.task
--- todos/todoapp/views.py
+++ todos/todoapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Mytodo
+from .forms import TodoForm
 
 # Create your views here.
+def alltodos(request):
+    tasks = Mytodo.objects.all()
+    form = TodoForm()
+
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+
+    return render(request, 'alltodo.html',  {'task': tasks, 'form': form} )
